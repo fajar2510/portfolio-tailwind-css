1bdbee66d3bafa8e3835984b95204ceeba272854
Fajar Abdurrohman <48176273+fajar2510@users.noreply.github.com>
2023-04-24 13:37:48 +0700

    add-dark-mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -34,3 +34,29 @@ window.addEventListener("click", function (e) {
     navMenu.classList.add("hidden");
   }
 });
+
+// darlmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindah toggle sesuai mode
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
